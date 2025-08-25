1edcb9c95fa0dba190af76d76d68291d99f08ad9
NicolasMerenco <nicolas.mel2601@gmail.com>
2025-08-25 13:49:56 -0300

    Está funcionando?

---
+++ vetores2.py
@@ -0,0 +1,13 @@
+#semana 21. Vetores empython(lista)
+
+vector_inteiros=[1,2,3,4,5]
+vector_misto=['a',2,3,True,'Python']
+
+#Acesso a elementos do vetor. O acesso aos elemetos é feito a síntexe vetor [indice] Exemplo:
+
+primeiro_elemento=vector_inteiros[0] #Acesso ao primeiro elemento.
+terceiro_elemento=vector_inteiros [2] #Acesso ao terceiro elemento.
+
+
+
+
